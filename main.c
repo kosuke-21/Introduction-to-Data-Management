6ecf971cfeec9c74131be1dc25788bf7eb1a1171
kosuke-21 <s1310233@u-aizu.ac.jp>
2024-07-04 14:11:51 +0900

    Final revision

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-void coinToss() {
+void coinToss(char* name) {
     int heads = 0, tails = 0;
     char* results[3];
     srand(time(0));
@@ -19,14 +20,21 @@ void coinToss() {
         printf("ラウンド %d: %s\n", i + 1, results[i]);
     }
     printf("表: %d, 裏: %d\n", heads, tails);
+
+    if (heads > tails) {
+        printf("%sが勝ちました！\n", name);
+    } else {
+        printf("%sが負けました！\n", name);
+    }
 }
 
 int main() {
     char name[50];
     printf("あなたの名前は？\n> ");
     fgets(name, 50, stdin);
+    name[strcspn(name, "\n")] = 0;  // Remove the newline character
     printf("こんにちは、%s!\n", name);
 
-    coinToss();
+    coinToss(name);
     return 0;
 }
